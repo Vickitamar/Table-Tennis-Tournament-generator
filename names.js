1771716e63ef88c7f772994c125d0b832a95c860
Vicki Edwards <vicki-tamar@hotmail.com>
2018-04-03 14:07:14 +0100

    added a diagram

--- names.js
+++ names.js
@@ -29,10 +29,7 @@
 		let li = document.createElement("li");
 		li.textContent = el;
 		itemsFragment.appendChild(li);
-		let span = document.createElement("span");
-		let newContent = document.createTextNode("X");
-		span.appendChild(newContent);
-		itemsFragment.appendChild(span);
+		
 	});
 		
 		list.appendChild(itemsFragment);
